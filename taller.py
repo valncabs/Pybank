6d6061c888b006a1e3543ceaeef9e3ba8d07b089
Kevin Mercado <141512106+KevinMercado1@users.noreply.github.com>
2026-02-25 13:10:56 -0500

    trying.

--- taller.py
+++ taller.py
@@ -1,2 +1,10 @@
 saldo = 1000
 print("hola")
+
+def retirar_dinero(cantidad):
+    global saldo
+    if cantidad > saldo:
+        print("No tienes suficiente dinero")
+    else:
+        saldo -= cantidad
+        print("Has retirado", cantidad, "tu nuevo saldo es", saldo)
