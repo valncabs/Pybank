51ad7481d9fe43a1ff4ca510d6baeb44535ede7c
sebarebar <sebarebar@gmail.com>
2026-02-26 07:21:07 -0500

    cargar archivo withdraw

---
+++ C3-feature_withdraw_money.py
@@ -0,0 +1,14 @@
+elif opcion == "3":
+        #opcion 3
+        monto = float(input("ingrese el monto que desea depositar: "))
+        while monto < 0:
+            monto = float(input("ingrese una cantidad valida, vuelva intentar"))
+        
+        if monto > 0:
+            saldo += monto
+            print(f"usted ingreso con exito {saldo} a su cuenta")
+    
+    else:
+        #validacion de las opciones
+        print ("opcion invalida")
+        
