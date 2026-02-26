1482217fd8e0418913ef67d59a075ccfb3c6e313
Kevin <kevinmercado1912000@icloud.com>
2026-02-26 07:20:59 -0500

    correción en While.

--- Deposit.py
+++ Deposit.py
@@ -2,7 +2,7 @@
         #opcion 3
         monto = float(input("ingrese el monto que desea depositar: "))
         while monto < 0:
-            monto = float(input("ingrese una cantidad valida, vuelva intentar"))
+            monto = float(input("ingrese una cantidad valida, vuelva intentar: "))
         
         if monto > 0:
             saldo += monto
