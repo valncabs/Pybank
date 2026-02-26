dcd8c833f2545d4d5dce43785fef65c6d6ee0439
dilandcv <dilandavidchavez123@gmail.com>
2026-02-26 07:16:19 -0500

    agrgando print de saldo

--- check_balance.py
+++ check_balance.py
@@ -1,5 +1,7 @@
 #opcion 1 
 #Luis Reyes // Brayan Lozada // Dilan Chavez
-opcion = 0
-if opcion == 1:
- print ("hola")
+saldo = 1000
+opcion = "1"
+
+if opcion == "1":
+ print ("su saldo es", saldo)
